955903600f1093a1507659327f61437869687141
Sam0294 <sam_f94@hotmail.com>
2022-04-19 19:11:03 +0000

    subo ejercicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,32 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [selected, setSelected] = useState("");
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="rounded-0 container p-10 box">
+			<div
+				className={`rounded-circle bg-danger red ${
+					selected == "red" ? " glow" : ""
+				} `}
+				onClick={() => {
+					setSelected("red");
+				}}></div>
+			<div
+				className={`rounded-circle bg-warning yellow ${
+					selected == "yellow" ? " glow" : ""
+				} `}
+				onClick={() => {
+					setSelected("yellow");
+				}}></div>
+			<div
+				className={`rounded-circle bg-success green ${
+					selected == "green" ? " glow" : ""
+				} `}
+				onClick={() => {
+					setSelected("green");
+				}}></div>
 		</div>
 	);
 };
